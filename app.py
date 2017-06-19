36a8f0c8586da716b3b5df83d68ade5978ca47d8
shinocal <nickmontgomeryjames@gmail.com>
2017-06-19 15:58:22 +0000

    6-19-17-1158am

--- app.py
+++ app.py
@@ -25,9 +25,9 @@ def index():
       enddate='%s%s%s' % (i.year,i.month,i.day)
       user_input=request.form['tkr']
 
-    if len(str(i.month))==1:
-      startdate='%s%s%s%s' % (i.year,0,i.month-1,i.day)
-      enddate='%s%s%s%s' % (i.year,0,i.month,i.day)
+#    if len(str(i.month))==1:
+#      startdate='%s%s%s%s' % (i.year,0,i.month-1,i.day)
+#      enddate='%s%s%s%s' % (i.year,0,i.month,i.day)
 
     api_url='https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?date.gte=%s&date.lt=%s&ticker=%s&api_key=MUSMX7hgtssoAwLpwFnb' % (startdate,enddate,user_input) 
 
