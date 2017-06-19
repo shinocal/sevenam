75e73342da67724c8731dd1917f1f109f6484766
shinocal <nickmontgomeryjames@gmail.com>
2017-06-19 21:47:49 +0000

    6-19-17

--- app.py
+++ app.py
@@ -18,9 +18,13 @@ def index():
       i=datetime.datetime.now()
 
       user_input=request.form['tkr']
-
-      startdate='%s%s%s%s' % (i.year,0,i.month-1,i.day)
-      enddate='%s%s%s%s' % (i.year,0,i.month,i.day)
+      
+      startdate='%s%s%s' % (i.year,i.month-1,i.day)
+      enddate='%s%s%s' % (i.year,i.month,i.day)
+      
+      if len(str(i.month))==1:
+        startdate='%s%s%s%s' % (i.year,0,i.month-1,i.day)
+        enddate='%s%s%s%s' % (i.year,0,i.month,i.day)
 
       api_url='https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?date.gte=%s&date.lt=%s&ticker=%s&api_key=MUSMX7hgtssoAwLpwFnb' % (startdate,enddate,user_input) 
 
@@ -29,16 +33,17 @@ def index():
       df=pd.DataFrame(data['datatable']['data'])
 
       closing_price=df[5]
+      dates=df[1]
 
       var=[];
 
       for n in range(len(closing_price)-1):
         var.append(closing_price[n])
 
-      x=range(1,len(closing_price))
+      #x=range(1,len(closing_price))
       p = figure(title="closing price", x_axis_label='x', y_axis_label='y')
-      p.line(x, var, legend="Temp.", line_width=2)
-      #save(p,filename="/templates/display.html")
+      p.line(dates, var, legend="Clos. Price", line_width=2)
+      save(p,filename="/templates/display.html")
       script, div = components(p)       
       return render_template('display.html', script=script, div=div)
       
